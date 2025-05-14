725642d12ea001096a433e41df936945238401a7
wsl_case <francema@ubuntu.lan>
2025-05-14 17:32:40 +0200

    gnl ok

--- libft/get_next_line/get_next_line_utils_bonus.c
+++ libft/get_next_line/get_next_line_utils_bonus.c
@@ -6,52 +6,11 @@
 /*   By: francema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 16:39:19 by francema          #+#    #+#             */
-/*   Updated: 2025/05/07 17:30:12 by francema         ###   ########.fr       */
+/*   Updated: 2025/05/14 17:32:15 by francema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*sub;
-	size_t	i;
-	size_t	true_len;
-
-	if (!s)
-		return (NULL);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	true_len = ft_strlen(s + start);
-	if (true_len > len)
-		true_len = len;
-	sub = malloc(sizeof(char) * (true_len + 1));
-	if (!sub)
-		return (NULL);
-	i = 0;
-	while (i < true_len)
-	{
-		sub[i] = s[start + i];
-		i++;
-	}
-	sub[i] = '\0';
-	return (sub);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	if (!s)
-		return (NULL);
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if (c == '\0')
-		return ((char *)s);
-	return (NULL);
-}
 
 char	*ft_strjoin_gnl(char *s1, char *s2)
 {
